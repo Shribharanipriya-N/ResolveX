c886bfbbf0abc507305803a91e0c5867ae8b3265
Pavitharanayaki24 <pavitharanayaki.k2022it@sece.ac.in>
2025-03-22 15:54:12 +0530

    project setup

---
+++ src/main/java/com/application/ResolveX/ResolveXApplication.java
@@ -0,0 +1,13 @@
+package com.application.ResolveX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResolveXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResolveXApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/application/ResolveX/ResolveXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.ResolveX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResolveXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
