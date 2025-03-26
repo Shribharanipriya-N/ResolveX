aad23d5d8c8480eaf14bad37fb8fbb34e99ac762
Shribharanipriya-N <shribharanipriya2004@gmail.com>
2025-03-27 00:06:13 +0530

    User API

--- src/main/java/com/application/ResolveX/ResolveXApplication.java
+++ src/main/java/com/application/ResolveX/ResolveXApplication.java
@@ -1,5 +1,6 @@
 package com.application.ResolveX;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -10,7 +11,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class ResolveXApplication {
 
 	public static void main(String[] args) {
-
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USER", dotenv.get("DB_USER"));
+		System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
 		SpringApplication.run(ResolveXApplication.class, args);
 	}
 
